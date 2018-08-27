dbea3eacb3b80c63f58ee0e4791e01ab99eb982e
Jalak <vorajalak@yahoo.co.in>
2018-08-27 21:58:06 +0530

    Testing Linting

--- public/index.js
+++ public/index.js
@@ -1,11 +1,10 @@
-
-import React from "react";
-import ReactDOM from "react-dom";
-import style from './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import style from './index.css';
+              var a = 5;
 
 const Index = () => {
-    return <div className={style.title}>Hello  React !
-    My name is </div>;
+  return <div className={style.title}>Hello React ! My name is </div>;
 };
 
-ReactDOM.render(<Index />, document.getElementById("root"));
+ReactDOM.render(<Index />, document.getElementById('root'));

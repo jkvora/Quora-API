9fb98f59db0d8eb6523ed64b0d882661545c95d8
jkvora <vorajalak@yahoo.co.in>
2018-08-27 21:59:45 +0530

    Testing Precommit script

--- public/index.js
+++ public/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import style from './index.css';
-              var a = 5;
+import React from "react";
+import ReactDOM from "react-dom";
+import style from "./index.css";
+var a = 5;
 
 const Index = () => {
   return <div className={style.title}>Hello React ! My name is </div>;
 };
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+ReactDOM.render(<Index />, document.getElementById("root"));

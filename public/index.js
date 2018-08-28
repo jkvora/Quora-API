3492eec59ab27cf394c7b90e71f2b539d8bef2e6
Jalak <vorajalak@yahoo.co.in>
2018-08-28 08:50:44 +0530

    Changes For development structure

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import style from "./index.css";
-var a = 5;
+
 
 const Index = () => {
   return <div className={style.title}>Hello React ! My name is </div>;
--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,66 +1,69 @@
 const path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
-    mode: 'development',
-    devtool: 'source-map',
-    entry: './index.js',
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './index.js',
 
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
-    },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js'
+  },
 
-    plugins: [new HtmlWebpackPlugin({
-        title: 'Quora-API',
-        filename: 'index.html',
-        template: 'index.html'
-    })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Quora-API',
+      filename: 'index.html',
+      template: 'index.html'
+    })
+  ],
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true,
-                            importLoaders: 1,
-                            localIdentName: "[name]_[local]_[hash:base64]",
-                            sourceMap: true,
-                            minimize: true
-                        }
-                    }
-                ]
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[name]_[local]_[hash:base64]',
+              sourceMap: true,
+              minimize: true
             }
+          }
         ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, '/public'), // serve your static files from here
-        watchContentBase: true, // initiate a page refresh if static content changes
-        proxy: [ // allows redirect of requests to webpack-dev-server to another destination
-            {
-                context:['/profile'],
-                target: 'http://localhost:5000', // server and port to redirect to
-                secure: false,
-            },
-        ],
-        port: 3030, // port webpack-dev-server listens to, defaults to 8080
-        overlay: { // Shows a full-screen overlay in the browser when there are compiler errors or warnings
-            warnings: false, // defaults to false
-            errors: false, // defaults to false
-        },
-    },
-}
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '/public'), // serve your static files from here
+    watchContentBase: true, // initiate a page refresh if static content changes
+    proxy: [
+      // allows redirect of requests to webpack-dev-server to another destination
+      {
+        context: ['/profile'],
+        target: 'http://localhost:5000', // server and port to redirect to
+        secure: false
+      }
+    ],
+    port: 3030, // port webpack-dev-server listens to, defaults to 8080
+    overlay: {
+      // Shows a full-screen overlay in the browser when there are compiler errors or warnings
+      warnings: false, // defaults to false
+      errors: false // defaults to false
+    }
+  }
+};

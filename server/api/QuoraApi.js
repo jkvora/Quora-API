3ab73774d6c27a5836cb166ba7c94633c86d2c21
jkvora <vorajalak@yahoo.co.in>
2018-10-14 15:21:00 +0530

    Api error handling

--- server/api/QuoraApi.js
+++ server/api/QuoraApi.js
@@ -1,74 +1,100 @@
-var express = require('express');
-var puppeteer = require('puppeteer');
+var express = require("express");
+var puppeteer = require("puppeteer");
 var router = express.Router();
-
-
 
 var qURL = "https://www.quora.com/";
 
-
 //Get profile Details
-router.get('/profile/:username', async function (req, res) {
-
+router.get("/profile/:username", async function(req, res) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(qURL + "profile/" + req.params.username);
+  let outputJson = {};
+  try {
+    await page.goto(qURL + "profile/" + req.params.username);
+    outputJson = await page.evaluate(() => {
+      var profile = {};
 
+      profile.work = document.querySelector(
+        ".WorkCredentialListItem  .UserCredential"
+      ).innerText;
+      profile.workdesc = document.querySelector(
+        ".WorkCredentialListItem  .detail_text"
+      ).innerText;
 
-  const data = await page.evaluate(() => {
-    var profile = {};
+      profile.school = document.querySelector(
+        ".SchoolCredentialListItem  .UserCredential"
+      ).innerText;
 
-    profile.work = document.querySelector(".WorkCredentialListItem  .UserCredential").innerText;
-    profile.workdesc = document.querySelector(".WorkCredentialListItem  .detail_text").innerText;
-
-    profile.school = document.querySelector(".SchoolCredentialListItem  .UserCredential").innerText;
-
-    profile.location = document.querySelector(".LocationCredentialListItem  .UserCredential").innerText;
-    return profile;
-  });
+      profile.location = document.querySelector(
+        ".LocationCredentialListItem  .UserCredential"
+      ).innerText;
+      return profile;
+    });
+  } catch (err) {
+    outputJson = {
+      error: "Something went wrong"
+    };
+  }
   await browser.close();
-  res.send(data);
+  res.send(outputJson);
 });
 
-
 //Get Stats Detials
-router.get('/stats/:username', async function (req, res) {
-
+router.get("/stats/:username", async function(req, res) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(qURL + "profile/" + req.params.username);
+  let outputJson = {};
+  try {
+    await page.goto(qURL + "profile/" + req.params.username);
+    outputJson = await page.evaluate(() => {
+      var stats = {};
+      stats.answers = Number(
+        document.querySelector(".AnswersNavItem  .list_count").innerText
+      );
+      stats.questions = Number(
+        document.querySelector(".QuestionsNavItem  .list_count").innerText
+      );
 
+      stats.posts = Number(
+        document.querySelector(".PostsNavItem  .list_count").innerText
+      );
+      stats.blogs = Number(
+        document.querySelector(".BlogsNavItem  .list_count").innerText
+      );
+      stats.followers = Number(
+        document.querySelector(".FollowersNavItem  .list_count").innerText
+      );
 
-  const data = await page.evaluate(() => {
-    var stats = {};
-    stats.answers = document.querySelector(".AnswersNavItem  .list_count").innerText;
-    stats.questions = document.querySelector(".QuestionsNavItem  .list_count").innerText;
+      stats.following = Number(
+        document.querySelector(".FollowingNavItem  .list_count").innerText
+      );
+      stats.topics = Number(
+        document.querySelector(".TopicsNavItem  .list_count").innerText
+      );
+      stats.edits = Number(
+        document.querySelector(".OperationsNavItem  .list_count").innerText
+      );
 
-    stats.posts = document.querySelector(".PostsNavItem  .list_count").innerText;
-    stats.blogs = document.querySelector(".BlogsNavItem  .list_count").innerText;
-    stats.followers = document.querySelector(".FollowersNavItem  .list_count").innerText;
-
-    stats.following = document.querySelector(".FollowingNavItem  .list_count").innerText;
-    stats.topics = document.querySelector(".TopicsNavItem  .list_count").innerText;
-    stats.edits = document.querySelector(".OperationsNavItem  .list_count").innerText;
-
-    return stats;
-  });
+      return stats;
+    });
+  } catch (err) {
+    outputJson = {
+      error: "Something went wrong"
+    };
+  }
   await browser.close();
-  res.send(data);
-
+  res.send(outputJson);
 });
 
-
-
 //Get Stats Detials
-router.get('/answer/:question/:username', async function (req, res) {
-
+router.get("/answer/:question/:username", async function(req, res) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(qURL + req.params.question + "/answer/" + req.params.username, { "waitUntil": "networkidle0" });
-  await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
-
+  await page.goto(
+    qURL + req.params.question + "/answer/" + req.params.username,
+    { waitUntil: "networkidle0" }
+  );
+  await page.evaluate("window.scrollTo(0, document.body.scrollHeight)");
 
   await page.evaluate(() => {
     let body = document.querySelector("body");
@@ -79,26 +105,24 @@ router.get('/answer/:question/:username', async function (req, res) {
     body.innerHTML = "";
     body.appendChild(answer);
     body.appendChild(authorCard);
-
   });
 
-  res.setHeader('Content-disposition', 'attachment; filename=' + req.params.question + "  ( By - " + req.params.username + " ).pdf ");
-  res.setHeader('Content-type', 'application/pdf');
-  res.charset = 'UTF-8';
-
+  res.setHeader(
+    "Content-disposition",
+    "attachment; filename=" +
+      req.params.question +
+      "  ( By - " +
+      req.params.username +
+      " ).pdf "
+  );
+  res.setHeader("Content-type", "application/pdf");
+  res.charset = "UTF-8";
 
   await page.pdf().then(data => {
     res.write(data);
   });
   await browser.close();
   res.send();
-
 });
-
-
-
-
-
-
 
 module.exports = router;
